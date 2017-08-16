0f755fc1638d04477c726e602c46486c5e978abc
dhpl <dieuhoangphilongit@gmail.com>
2017-08-16 10:06:50 +0700

    SQLiteDatabase, CursorWrapper

---
+++ app/src/main/java/com/philong/criminalintent/database/CrimeCursorWrapper.java
@@ -0,0 +1,34 @@
+package com.philong.criminalintent.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import com.philong.criminalintent.Crime;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Long on 8/16/2017.
+ */
+
+public class CrimeCursorWrapper extends CursorWrapper {
+
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Crime getCrime(){
+        String uuid = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.SOLVED));
+        String suspect = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.SUSPECT));
+        Crime crime = new Crime(UUID.fromString(uuid));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+        crime.setSuspect(suspect);
+        return crime;
+    }
+}
---
+++ app/src/main/java/com/philong/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,20 @@
+package com.philong.criminalintent.database;
+
+/**
+ * Created by Long on 8/16/2017.
+ */
+
+public class CrimeDbSchema {
+
+    public static final class CrimeTable{
+        public static final String NAME = "crimes";
+        public static final class Cols{
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+            public static final String SUSPECT = "suspect";
+        }
+    }
+
+}
